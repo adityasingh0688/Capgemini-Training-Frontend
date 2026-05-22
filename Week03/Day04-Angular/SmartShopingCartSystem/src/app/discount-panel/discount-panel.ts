1f0b5af9b2c94eb6cc0f26242c904193d75a5b54
Aditya <aditya@Adityas-MacBook-Air.local>
2026-05-22 09:47:44 +0530

    Week03 Day4

---
+++ Week03/Day04-Angular/SmartShopingCartSystem/src/app/discount-panel/discount-panel.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-discount-panel',
+  imports: [],
+  templateUrl: './discount-panel.html',
+  styleUrl: './discount-panel.css',
+})
+export class DiscountPanel {}
