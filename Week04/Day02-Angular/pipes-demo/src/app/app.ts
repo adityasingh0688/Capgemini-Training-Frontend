01ec8698406de149d641ff5311a476bad0a284c2
Aditya <aditya@Adityas-MacBook-Air.local>
2026-05-26 14:49:40 +0530

    Till-PipesInAngular

---
+++ Week04/Day02-Angular/pipes-demo/src/app/app.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  imports: [CommonModule],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+
+export class App {
+
+  name:string = "angular pipes demo";
+
+  cities:string[] = [
+    "delhi",
+    "mumbai",
+    "bangalore",
+    "pune"
+  ];
+
+  employee = {
+
+    id:101,
+    name:"Aditya",
+    department:"IT",
+    salary:50000
+  };
+
+  currentDate:Date = new Date();
+
+  price:number = 1234.5678;
+
+  discount:number = 0.25;
+
+  message:string = "Welcome To Angular Pipes";
+}
